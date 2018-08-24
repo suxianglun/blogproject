3b23ea77661d69ecaaaf850ae399134969aa298a
suxianglun <suxianglun@163.com>
2018-08-24 16:44:22 +0800

    统计分类下文章数

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -6,6 +6,7 @@
 # @Version : 
 from django import template
 from ..models import Post, Category, Tag
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -22,7 +23,10 @@ def get_archives():
 
 @register.simple_tag()
 def get_categories():
-    return Category.objects.all()
+    # Count()计算各个分类下文章数
+    # post 告诉Count（）是Post模型
+    # gt 就是 greate than，表示大于，不是大于等于。大于等于是 gte
+    return Category.objects.annotate(nums_posts=Count('post')).filter(nums_posts__gt=0)
 
 
 @register.simple_tag()
